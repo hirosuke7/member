b74cd56fbd9410c418002654d067ea5bd6e678f1
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-09 08:15:33 +0900

    fix6

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,11 +1,6 @@
 class MemberController < ApplicationController
 
-
-
-
   def index
-
-
 
     @members = [{id: 1, name: "島田 隆", sex: 0, age: 21 ,signal:nil},
                 {id: 2, name: "小森 潤一", sex: 0, age: 19,signal:nil},
@@ -40,7 +35,19 @@ class MemberController < ApplicationController
 
 
   def show
-    @id = params[:id].to_i
+  @members = [{id: 1, name: "島田 隆", sex: 0, age: 21 ,signal:nil},
+              {id: 2, name: "小森 潤一", sex: 0, age: 19,signal:nil},
+              {id: 3, name: "松井 早苗", sex: 1, age: 20,signal:nil},
+              {id: 4, name: "郡司 幸男", sex: 0, age: 18,signal:nil},
+              {id: 5, name: "菅原 遥", sex: 1, age: 30,signal:nil},
+              {id: 6, name: "辻村 佳代子", sex: 1, age: 15,signal:nil},
+              {id: 7, name: "今 亜希", sex: 1, age: 23,signal:nil},
+              {id: 8, name: "間瀬 覚", sex: 0, age: 44,signal:nil}]
+
+
+  @members = @members.select{|item|item[:id] == params[:id].to_i}
+
+
   end
 
 end
